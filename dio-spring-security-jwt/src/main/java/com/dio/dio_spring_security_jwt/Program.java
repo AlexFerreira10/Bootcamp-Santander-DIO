63d5267a9f54694a30f6ceb94b47c00b36787271
AlexFerreira10 <correaferreiraalex92@email.com>
2024-07-06 18:25:04 -0300

    Autenticação simples funcionando

--- dio-spring-security-jwt/src/main/java/com/dio/dio_spring_security_jwt/Program.java
+++ dio-spring-security-jwt/src/main/java/com/dio/dio_spring_security_jwt/Program.java
@@ -2,13 +2,12 @@ package com.dio.dio_spring_security_jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class DioSpringSecurityJwtApplication {
+public class Program {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DioSpringSecurityJwtApplication.class, args);
+		SpringApplication.run(Program.class, args);
 	}
 
 }
--- dio-spring-security-jwt/src/main/java/com/dio/dio_spring_security_jwt/config/WebSecurityConfig.java
+++ dio-spring-security-jwt/src/main/java/com/dio/dio_spring_security_jwt/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.dio.dio_spring_security_jwt.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,53 +9,57 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+@Configuration // Indica que esta classe contém configurações de beans.
+@EnableWebSecurity // Habilita a segurança web.
 public class WebSecurityConfig {
 
-    @Bean
+    @Bean // Define um bean que será gerenciado pelo Spring.
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .securityMatcher("/api/**", "/app/**") // Define os padrões de URL para aplicar a configuração de segurança
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers(HttpMethod.GET, "/").permitAll()
+                                // Permite acesso público ao endpoint "/".
+                                .requestMatchers(HttpMethod.GET, "/", "/login").permitAll()
+                                // Permite acesso público ao endpoint "/login".
                                 .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                                // Restringe o acesso ao endpoint "/users" para usuários com os papéis "USERS" ou "MANAGERS".
                                 .requestMatchers(HttpMethod.GET, "/users").hasAnyRole("USERS", "MANAGERS")
+                                // Restringe o acesso ao endpoint "/managers" para usuários com o papel "MANAGERS".
                                 .requestMatchers(HttpMethod.GET, "/managers").hasRole("MANAGERS")
+                                // Requer autenticação para qualquer outra solicitação.
                                 .anyRequest().authenticated()
                 )
-                .formLogin(formLogin ->
-                        formLogin
-                                .loginPage("/login")
-                                .permitAll()
-                );
-
-        return http.build();
+                .formLogin(formLogin -> formLogin.permitAll()); // Habilita a página de login padrão do Spring Security.
+        return http.build(); // Constrói o objeto SecurityFilterChain.
     }
 
-    @Bean
+    @Bean // Define um bean para UserDetailsService que será gerenciado pelo Spring.
     public UserDetailsService userDetailsService() {
+        // Cria um usuário com o nome de usuário "user", senha "user123" e papel "USERS".
         UserDetails user = User.withUsername("user")
-                .password("user123")
+                .password(passwordEncoder().encode("user123"))
                 .roles("USERS")
                 .build();
 
+        // Cria um administrador com o nome de usuário "admin", senha "admin123" e papel "MANAGERS".
         UserDetails admin = User.withUsername("admin")
-                .password("admin123")
+                .password(passwordEncoder().encode("admin123"))
                 .roles("MANAGERS")
                 .build();
 
+        // Retorna um gerenciador de usuários em memória com os dois usuários criados acima.
         return new InMemoryUserDetailsManager(user, admin);
     }
 
-    @Bean
+    @Bean // Define um bean para PasswordEncoder que será gerenciado pelo Spring.
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        // Retorna um codificador de senhas que utiliza o algoritmo BCrypt.
+        return new BCryptPasswordEncoder();
     }
 }
+
